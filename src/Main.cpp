9d172e035786302f03beeaa6fe3103677f8796d9
Halil-pgh <halil.pgh@gmail.com>
2021-02-09 19:21:16 +0300

    Crack passwords without reopenning program!

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,13 +7,20 @@
 
 int main()
 {
-    std::cout << "What is your password: ";
-    std::string pass;
-    std::cin >> pass;
-    Password password(pass);
+    char repait;
+    do
+    {
+        std::cout << "What is your password: ";
+        std::string pass;
+        std::cin >> pass;
+        Password password(pass);
 
-    Timer timer("Password cracking");
-    Cracker cracker;
-    cracker.Crack(password);
-    timer.End();
+        Timer timer("Password cracking");
+        Cracker cracker;
+        cracker.Crack(password);
+        timer.End();
+
+        std::cout << "Do you want to crack another password [Y/n]: ";
+        std::cin >> repait;
+    } while (repait == 'y' || repait == 'Y');
 }
